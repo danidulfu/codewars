e83bcca54146860328837616357acfe9c4e9a3b0
danidulfu <dani.codelocal@gmail.com>
2024-11-23 11:22:05 +0000

    add kata

---
+++ kyu8/welcome/solution.js
@@ -0,0 +1,27 @@
+function greet(language) {
+	let greetings = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  if(greetings.hasOwnProperty(language)) {
+    return greetings[language];
+  }
+  else {
+    return 'Welcome';
+  }
+}
