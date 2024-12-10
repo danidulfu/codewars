ce64a2f1eadb6489f674d3c224dbce9ec193af3a
danidulfu <dani.codelocal@gmail.com>
2024-12-10 17:32:39 +0000

    kata upload

--- kyu7/fix_string_case/solution.js
+++ kyu7/fix_string_case/solution.js
@@ -5,20 +5,5 @@ function solve(s){
       if (s[i].toUpperCase()==s[i]) {uppercaseCount+=1}
       else {lowercaseCount+=1}
     }
-    let result=''
-    if (lowercaseCount<uppercaseCount){
-      for (let i=0;i<s.length;i++){
-        if (s[i].toLowerCase()==s[i]) {
-          result+=s[i].toUpperCase()
-        } else result+=s[i]
-      }
-      return result
-    } else {
-      for (let i=0;i<s.length;i++){
-        if (s[i].toUpperCase()==s[i]) {
-          result+=s[i].toLowerCase()
-        } else result+=s[i]
-      }
-      return result
-    }
+    return lowercaseCount<uppercaseCount ? s.toUpperCase() : s.toLowerCase()
   }
