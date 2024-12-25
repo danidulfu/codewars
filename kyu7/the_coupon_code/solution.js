659727129a8dea7d768fa6ac76de8942183a8237
danidulfu <dani.codelocal@gmail.com>
2024-12-25 11:17:30 +0000

    kata upload

---
+++ kyu7/the_coupon_code/solution.js
@@ -0,0 +1,5 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    currentDate=new Date(currentDate)
+    expirationDate=new Date(expirationDate)
+    return ((enteredCode===correctCode)&&(currentDate<=expirationDate))
+  }
