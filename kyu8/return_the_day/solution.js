59ab4f5e36d51c15c08c17343e75e3cdf805629e
danidulfu <dani.codelocal@gmail.com>
2025-01-14 14:05:17 +0000

    kata upload

---
+++ kyu8/return_the_day/solution.js
@@ -0,0 +1,6 @@
+function whatday(num) { 
+    let days={1:'Sunday',2:'Monday',3:'Tuesday',4:'Wednesday',5:'Thursday',6:'Friday',7:'Saturday'}
+    if ((num<1)||(num>7)) {
+      return 'Wrong, please enter a number between 1 and 7'
+    } else return days[num]
+  }
