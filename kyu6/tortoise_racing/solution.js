1743d229242bc616cdae209d20f4a10ce0c49b53
danidulfu <dani.codelocal@gmail.com>
2025-01-24 12:53:16 +0000

    kata upload

---
+++ kyu6/tortoise_racing/solution.js
@@ -0,0 +1,8 @@
+function race(v1, v2, g) {
+    if (v1>=v2) return null
+     let time=g/(v2-v1)
+     let hours=Math.floor(time)
+     let minutes=Math.floor((time*60)%60)
+     let seconds=Math.floor((time*3600)%60)
+     return [hours, minutes, seconds]
+  }
